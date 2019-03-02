486217854b68befb70580be892a159ef7e6063be
Marcos García Yeh <antixsuperstar@gmail.com>
2019-03-02 17:30:20 -0600

    First attempt of solution to project!

--- src/App.vue
+++ src/App.vue
@@ -16,26 +16,28 @@
                 </div>
                 <div class="row text-center">
                     <div class="col">
-                        <textarea class="form-control"></textarea><br />
-                        <button class="btn btn-primary">Add Quote</button>
+                        <textarea
+                                class="form-control"
+                                v-model="newQuote"
+                                id="newquotecontainer"
+                                placeholder="Write something inspiring..."></textarea><br />
+                        <button
+                                class="btn btn-primary"
+                                @click="addQuote">Add Quote</button>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row mt-4"> <!-- Quote listing -->
-            <div
-                    v-for="(q, i) in quotes"
-                    class="col-6 col-md-2 quote">
-                <p
-                        @click="removeQuote"
-                        :qid="i"
-                        class="text-center">{{ q }}</p>
-            </div>
+            <quote v-for="(q, i) in quotes" :i="i">{{ q }}</quote>
         </div>
     </div>
 </template>
 
 <script>
+    import { eventbus } from "./main.js";
+    import Quote from "./components/Quote.vue";
+
     export default {
         data: function() {
             return {
@@ -43,15 +45,39 @@
                     'This is a quote',
                     'This is another quote',
                     'This is a much much longer quote'
-                ]
+                ],
+                newQuote: ''
             }
         },
+        components: {
+            Quote
+        },
         methods: {
-            removeQuote: function(event) {
-                console.log(event.target.$attrs('qid'));
+            addQuote: function() {
+                // Let's do some basic validation
+                switch (true) {
+                    case !this.newQuote.length: // There is no text to add
+                        alert('Add some text before clicking the button :)');
+                        break;
+                    case this.quotes.length==10: // Reached maximum!
+                        alert('I can only add up to ten quotes :(');
+                        break;
+                    default: // Nothing unusual, let's add that wonderful quote!
+                        this.quotes.push(this.newQuote);
+                        this.newQuote = '';
+                        break;
+                }
+                document.getElementById('newquotecontainer').focus();
             }
+        },
+        created() {
+            eventbus.$on('removeQuote', event => {
+                let id = event.target.getAttribute('qid');
+                this.quotes.splice(id, 1);
+            });
         }
     }
+
 </script>
 
 <style scoped>
@@ -62,12 +88,5 @@
         border: 1px;
         border-radius: 5px;
         content:'fdsa'
-    }
-    div.quote > p {
-        cursor: pointer;
-        font-family: 'Comic Sans MS';
-        border: thin solid lightgray;
-        border-radius: 0.5rem;
-        padding: 1rem;
     }
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-6 col-md-2 quote">
+        <p
+                @click="removeQuote"
+                :qid="i"
+                class="text-center">
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+    import { eventbus } from "../main";
+
+    export default {
+        props: [
+            'i'
+        ],
+        methods: {
+            removeQuote: function(event) {
+                eventbus.$emit('removeQuote', event);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div.quote > p {
+        cursor: pointer;
+        font-family: 'Comic Sans MS', sans-serif;
+        border: thin solid lightgray;
+        border-radius: 0.5rem;
+        padding: 1rem;
+    }
+</style>
